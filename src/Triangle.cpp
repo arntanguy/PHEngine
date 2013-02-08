767bbf0dfd0d084f219dbcb1136903c4b6048849
TANGUY Arnaud <arn.tanguy@gmail.com>
2013-02-08 21:39:47 +0000

    Fix Voronoi (part 2/2)

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -76,51 +76,60 @@ float Triangle::closestPointOnTriangleToPointVoronoi(const glm::vec3 &point, boo
 
     glm::vec3 edge[2];
     int i=0;
-    glm::vec3 AX, AB, AC, BC, BX, BA;
+    glm::vec3 AX, AB, AC, BC, BX;
     while(i < 3) {
-        std::cout << "point: " << mPoints[i].x << " " << mPoints[i].y << " " << mPoints[i].z << std::endl;
-        if(i==0) {
-            AX = point-mPoints[0];
-            BX = point-mPoints[1];
-            AB = mPoints[1]-mPoints[0];
-            AC = mPoints[2]-mPoints[0];
-            BC = mPoints[2]-mPoints[1];
-            edge[0] = mPoints[1];
-            edge[1] = mPoints[0];
-        } else if(i == 1) {
-            AX = point-mPoints[1];
-            BX = point-mPoints[2];
-            AB = mPoints[2]-mPoints[1];
-            BA = mPoints[1]-mPoints[2];
-            AC = mPoints[0]-mPoints[1];
-            BC = mPoints[0]-mPoints[2];
-            edge[0] = mPoints[2];
-            edge[1] = mPoints[1];
-        } else if(i==2) {
-            AX = point-mPoints[2];
-            BX = point-mPoints[0];
-            AB = mPoints[0]-mPoints[2];
-            AC = mPoints[1]-mPoints[2];
-            BC = mPoints[1]-mPoints[0];
+        AX = point-mPoints[i%3];
+        BX = point-mPoints[(i+1)%3];
+        AB = mPoints[(i+1)%3]-mPoints[i%3];
+        AC = mPoints[(i+2)%3]-mPoints[i%3];
+        BC = mPoints[(i+2)%3]-mPoints[(i+1)%3];
+        if(i<2) {
+            edge[i%3] = mPoints[(i+1)%3];
+            edge[(i+1)%3] = mPoints[i%3];
+        } else {
             edge[0] = mPoints[0];
             edge[1] = mPoints[2];
-            ////edge[0] = mPoints[0];
-            ////edge[1] = mPoints[2];
         }
+        // Just for helping with understanding
+        //if(i==0) {
+        //AX = point-mPoints[0];
+        //BX = point-mPoints[1];
+        //AB = mPoints[1]-mPoints[0];
+        //AC = mPoints[2]-mPoints[0];
+        //BC = mPoints[2]-mPoints[1];
+        //edge[0] = mPoints[1];
+        //edge[1] = mPoints[0];
+        //} else if(i == 1) {
+        //AX = point-mPoints[1];
+        //BX = point-mPoints[2];
+        //AB = mPoints[2]-mPoints[1];
+        //BA = mPoints[1]-mPoints[2];
+        //AC = mPoints[0]-mPoints[1];
+        //BC = mPoints[0]-mPoints[2];
+        //edge[0] = mPoints[2];
+        //edge[1] = mPoints[1];
+        //} else if(i==2) {
+        //AX = point-mPoints[2];
+        //BX = point-mPoints[0];
+        //AB = mPoints[0]-mPoints[2];
+        //AC = mPoints[1]-mPoints[2];
+        //BC = mPoints[1]-mPoints[0];
+        //edge[0] = mPoints[0];
+        //edge[1] = mPoints[2];
+        //////edge[0] = mPoints[0];
+        //////edge[1] = mPoints[2];
+        //}
 
-        std::cout << "i: " << i << std::endl;
         /**
          * Check vertex region
          **/
         if(glm::dot(AX, AB) <= 0 && glm::dot(AX, AC) <= 0) {
-                std::cout << "vertex region for i: " << i << std::endl;
-                bestPoint = mPoints[i];
-                inEdgeRegion = false;
-                break;
+            bestPoint = mPoints[i];
+            inEdgeRegion = false;
+            break;
         } else if(glm::dot(glm::cross(glm::cross(BC, -AB), -AB), BX) >= 0
                 && glm::dot(AX,AB) >= 0
                 && glm::dot(BX, -AB) >= 0) {
-                std::cout << "edge for i: " << i << std::endl;
             /**
              * Check edge region
              **/
@@ -133,8 +142,6 @@ float Triangle::closestPointOnTriangleToPointVoronoi(const glm::vec3 &point, boo
             bestPoint = edge[0] + glm::dot(point-edge[0], e)*e;
 
             break;
-        } else {
-            std::cout << "nothing for i: " << i << std::endl;
         }
         i++;
     }
@@ -146,10 +153,10 @@ float Triangle::closestPointOnTriangleToPointVoronoi(const glm::vec3 &point, boo
         glVertex3f(bestPoint.x, bestPoint.y, bestPoint.z);
         glEnd();
         if(inEdgeRegion) {
-                dt::drawLine(edge[0], edge[1], mNormal, 0.2, 0.2, glm::vec3(0., 0., 1.));
-                dt::drawPoint(bestPoint, 0.3);
+            dt::drawLine(edge[0], edge[1], mNormal, 0.2, 0.2, glm::vec3(0., 0., 1.));
+            dt::drawPoint(bestPoint, 0.3);
         } else {
-                dt::drawPoint(bestPoint, 0.3);
+            dt::drawPoint(bestPoint, 0.3);
         }
     }
 
